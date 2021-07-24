9dff288279eba359e9d77378e3d31599fd79f27e
ivanmaletic <ivan.maletic9821@gmail.com>
2021-07-24 22:24:26 +0200

    Movement, animations, clothing v1
    Added basic movement and animations of character. Also added 2 versions of each clothing items (blue and stock) which can be swapped via buttons

---
+++ Clothes shop/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public float speed = 4f;
+    public Animator animator;
+    Vector2 movement=new Vector2(0,0);
+    public Rigidbody2D rb;
+    public GameObject[] sides;
+ 
+    void Update()
+    {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (movement.x >= 0 && movement.y >= 0)
+        {
+            if (movement.x > movement.y)
+            {
+                SideActivator(1);
+            }
+            else
+            {
+                SideActivator(3);
+            }
+        }
+        if (movement.x<=0&&movement.y>=0) 
+        {
+            if (Mathf.Abs(movement.x) > movement.y) 
+            {
+                SideActivator(0);
+            }
+            else
+            {
+                SideActivator(3);
+            }
+        }
+        if (movement.x >= 0 && movement.y <= 0)
+        {
+            if (movement.x > Mathf.Abs(movement.y))
+            {
+                SideActivator(1);
+            }
+            else
+            {
+                SideActivator(2);
+            }
+        }
+        if (movement.x <= 0 && movement.y <= 0)
+        {
+            if (Mathf.Abs(movement.x) > Mathf.Abs( movement.y))
+            {
+                SideActivator(0);
+            }
+            else
+            {
+                SideActivator(2);
+            }
+        }
+    }
+    private void SideActivator(int j)
+    {
+        for (int i = 0; i < 4; i++) { sides[i].SetActive(false); }
+        sides[j].SetActive(true);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Clothes shop/Assets/Scripts/ClothesSelectionScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothesSelectionScript : MonoBehaviour
+{
+    [Header ("Sprites To Change")]
+    public List<SpriteRenderer> bodyPart=new List<SpriteRenderer>();
+
+    [Header("Sprites To Cycle Trough")]
+    public List<Sprite> options = new List<Sprite>();
+
+    private int currentOption = 0;
+
+    public void NextOption()
+    {
+        currentOption++;
+        if (currentOption >= options.Count) { currentOption = 0; }
+        foreach(SpriteRenderer bodypart in bodyPart)
+            bodypart.sprite = options[currentOption];
+    }
+    public void PreviousOption()
+    {
+        currentOption--;
+        if (currentOption <0) {currentOption= options.Count - 1; }
+        foreach (SpriteRenderer bodypart in bodyPart)
+            bodypart.sprite = options[currentOption];
+    }
+}
